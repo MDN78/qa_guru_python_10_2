2c341666a143d1401888aa3b31541bc4217ca8b3
Dmitry <dmitry-maksimov@yandex.ru>
2024-01-11 21:44:42 +0300

    update file conftest- fixture

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser
 
-@pytest.fixture
+@pytest.fixture(scope="function", autouse=True)
 def set_driver():
     browser.config.window_width = 1920
     browser.config.window_height = 720
